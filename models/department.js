04c1b78669b3c7a8ae3c7c4061261d72491816b4
Ss0110 <saraysanchez01@yahoo.com>
2023-06-15 18:56:13 -0700

    worked on department.js

---
+++ models/department.js
@@ -0,0 +1,81 @@
+const connection = require("../config/connection");
+
+class Department {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  static getAllDepartments() {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM departments";
+      connection.query(query, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          const departments = rows.map(
+            (row) => new Department(row.id, row.name)
+          );
+          resolve(departments);
+        }
+      });
+    });
+  }
+  static addDepartment(departmentName) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO departments (name) VALUES (?)";
+      connection.query(query, [departmentName], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.insertId);
+        }
+      });
+    });
+  }
+  static getDepartmentById(departmentId) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM departments WHERE id = ?";
+      connection.query(query, [departmentId], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (rows.length === 0) {
+            resolve(null); // Department not found
+          } else {
+            const department = new Department(rows[0].id, rows[0].name);
+            resolve(department);
+          }
+        }
+      });
+    });
+  }
+
+  updateDepartmentName(newName) {
+    return new Promise((resolve, reject) => {
+      const query = "UPDATE departments SET name = ? WHERE id = ?";
+      connection.query(query, [newName, this.id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.affectedRows > 0);
+        }
+      });
+    });
+  }
+
+  deleteDepartment() {
+    return new Promise((resolve, reject) => {
+      const query = "DELETE FROM departments WHERE id = ?";
+      connection.query(query, [this.id], (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.affectedRows > 0);
+        }
+      });
+    });
+  }
+}
+
+module.exports = Department;
